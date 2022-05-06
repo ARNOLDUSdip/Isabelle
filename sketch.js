8d6d378e1fa98b042a2c4bcb0fe49602a8ef8790
ARNOLDUSdip <105036255+ARNOLDUSdip@users.noreply.github.com>
2022-05-06 15:19:56 +0200

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -29,7 +29,7 @@ function draw() {
   if (hearts.length > 60) {
   	hearts.shift();
   }
-  textSize(width/20);
+  textSize(width/25);
   fill(0, 200, 200);
   textAlign(CENTER, CENTER);
   text('Sal jy my meisie wees Isabelle?',width/2,height/2);
